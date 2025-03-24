3f4535553084875fefc10d5bb6a8f37e3893e735
EvgenijK <komov.evgen@gmail.com>
2025-03-24 16:31:28 +0300

    Spell check, agenda adjustments

--- init.el
+++ init.el
@@ -2,3 +2,18 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("~/Documents/TomeNET.org" "/home/svechnik/Documents/Поиск новой работы.org" "/home/svechnik/Documents/Diary/diary_2025-03-23.org" "/home/svechnik/Documents/Diary/diary_2025-03-17.org" "/home/svechnik/Documents/Diary/diary_2025-03-12.org" "/home/svechnik/Documents/Diary/diary_2025-03-09.org" "/home/svechnik/Documents/Diary/diary_2025-03-08.org" "/home/svechnik/Documents/awesomewm.org" "/home/svechnik/Documents/Diary/diary_2025-03-07.org" "/home/svechnik/Documents/Diary/diary_2025-03-04.org" "/home/svechnik/Documents/Diary/diary_2025-03-03.org" "/home/svechnik/Documents/Work/work_tasks.org" "/home/svechnik/Documents/Diary/2025-03-02.org" "/home/svechnik/Documents/Diary/2025-03-01.org" "/home/svechnik/Documents/планы на 2025.org" "/home/svechnik/Documents/Agenda/README.org"))
+ '(package-selected-packages
+   '(guess-language reverse-im dashboard with-editor which-key use-package toc-org sudo-edit ssh-deploy php-mode org-bullets general doom-themes diminish diff-hl counsel company-box all-the-icons-ivy-rich all-the-icons-dired)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
